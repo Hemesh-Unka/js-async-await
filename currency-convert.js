79ad8599c38a0a1c739ec9dc09c6c13314a1bf2c
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-15 10:58:12 +0100

    added error handling to projet

--- currency-convert.js
+++ currency-convert.js
@@ -2,15 +2,28 @@ require('dotenv').config();
 const axios = require('axios');
 
 const getExchangeRate = async (from, to) => {
-  const response = await axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}`)
-  const euro = 1 / response.data.rates[from];
-  const rate = euro * response.data.rates[to];
-  return rate;
+  try {
+    const response = await axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}`)
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+
+    if (isNaN(rate)) {
+      throw new Error();
+    }
+
+    return rate;
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}.`)
+  }
 };
 
 const getCountries = async (currencyCode) => {
-  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-  return response.data.map((country) => country.name);
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+    return response.data.map((country) => country.name);
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`);
+  }
 };
 
 const convertCurrency = async (from, to, amount) => {
@@ -22,4 +35,6 @@ const convertCurrency = async (from, to, amount) => {
 
 convertCurrency('GBP', 'NZD', 100).then((message) => {
   console.log(message);
+}).catch((e) => {
+  console.log(e.message);
 });
